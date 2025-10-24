62b92283c14aa7eb7351ae3a13aa3a1e9c7f43b4
omar302 <omar3025@yahoo.com>
2025-10-24 11:36:42 +1100

    End of section 9

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -3,12 +3,21 @@
 import React from 'react'
 import { AiOutlineCar } from 'react-icons/ai'
 import { useParamsStore } from '../hooks/useParamsStore'
+import { usePathname, useRouter } from 'next/navigation';
 
 export default function Logo() {
+    const router = useRouter();
+    const pathname = usePathname();
+
     const reset = useParamsStore(state => state.reset)
-    
+
+    function handleReset() {
+        if (pathname !== '/') router.push('/');
+        reset();
+    }
+
     return (
-        <div onClick={reset} 
+        <div onClick={handleReset}
             className="cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500">
             <AiOutlineCar size={34} />
             <div>Carsties Auctions</div>
--- frontend/web-app/app/nav/NavBar.tsx
+++ frontend/web-app/app/nav/NavBar.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Logo from './Logo';
 import Search from './Search';
+import LoginButton from './LoginButton';
+import { getCurrentUser } from '../actions/authActions';
+import UserActions from './UserActions';
 
-export default function NavBar() {
+export default async function NavBar() {
+    const user = await getCurrentUser();
     return (
         <header className='sticky top-0 z-50 flex justify-between bg-white shadow-md py-5 px-5 items-center text-gray-800'>
             <Logo />
             <Search />
-            <div>Login</div>
+            {user ? (
+                <UserActions user={user}/>
+            ) : (
+                <LoginButton />
+            )}
         </header>
     );
 }
